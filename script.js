2f956364e6eab7d825e085a2a075eb52791b1ce1
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-15 11:33:56 -0300

    Gerando Senha

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("TESTE")
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePasswords = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPasswords = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%¨&*()-+-*/";
+let novaSenha = "";
+
+sizePasswords.innerHTML = sliderElement.ariaValueMax;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+
+    function copyPassword(){
+        alert("Senha copiada com sucesso!!!")
+        navigator.clipboard.writeText(novaSenha);
+    }
+}
